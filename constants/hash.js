8db36590bfcc48d6cf89dc3e266971c814ab8b2e
HaoHuynh0301 <hao152903@gmail.com>
2022-08-18 19:05:44 +0700

    feat: remove unmerged code.

--- constants/hash.js
+++
@@ -1,2 +0,0 @@
-export const UUIDV4_LENGTH = 37;
- 
--- index.js
+++
@@ -1,4 +0,0 @@
-import { createDocument } from './createDocument';
-import { verifyCardanoDocument } from './verifyDocument';
-
-export { createDocument, verifyCardanoDocument };
